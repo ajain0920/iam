7c02dc88d0bdfca71c191c5586e86ba28cc36f63
Abhinav <coolabhinav@gmail.com>
2026-01-17 11:23:01 +0530

    FedSSO example with KeyCloak IdP and OpenID connect

---
+++ src/main/java/com/learnig/fedssoapp/App.java
@@ -0,0 +1,16 @@
+package com.learnig.fedssoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+}
